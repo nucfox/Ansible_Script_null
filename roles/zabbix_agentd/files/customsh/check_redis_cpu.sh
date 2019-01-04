68df463b30e0b744df80b0d27bdf2c8987cc8c9e
liqingzhu <qingzhu.li@yunfangtang.com>
2019-01-04 11:23:07 +0800

    修改zabbix  redis脚本

--- roles/zabbix_agentd/files/customsh/check_redis_cpu.sh
+++ roles/zabbix_agentd/files/customsh/check_redis_cpu.sh
@@ -22,15 +22,15 @@ fi
 if [ "${redis_passwd}" ];then
         time_now=$(date +%s)
         result_now=$(${redis_cli_path} -a ${redis_passwd} -p ${redis_port} info|grep "${info_key}:"|cut -d: -f2)
-        time_before=$(cut -d: -f1 /tmp/redis_time_before_$redis_port)
-        result_before=$(cut -d: -f2 /tmp/redis_time_before_$redis_port)
+        time_before=$(cut -d: -f1 /tmp/redis_${info_key}_${redis_port})
+        result_before=$(cut -d: -f2 /tmp/redis_${info_key}_${redis_port})
         realres=$(awk -v rnow=$result_now -v rbe=$result_before -v tnow=$time_now -v tbe=$time_before 'BEGIN{printf "%.2f\n",(rnow-rbe)/(tnow-tbe)*100}')
         echo "$time_now:$result_now" > /tmp/redis_${info_key}_${redis_port}
 else
         time_now=$(date +%s)
         result_now=$(${redis_cli_path} -p ${redis_port} info|grep "${info_key}:"|cut -d: -f2)
-        time_before=$(cut -d: -f1 /tmp/redis_time_before_$redis_port)
-        result_before=$(cut -d: -f2 /tmp/redis_time_before_$redis_port)
+        time_before=$(cut -d: -f1 /tmp/redis_${info_key}_${redis_port})
+        result_before=$(cut -d: -f2 /tmp/redis_${info_key}_${redis_port})
         realres=$(awk -v rnow=$result_now -v rbe=$result_before -v tnow=$time_now -v tbe=$time_before 'BEGIN{printf "%.2f\n",(rnow-rbe)/(tnow-tbe)*100}')
         echo "$time_now:$result_now" > /tmp/redis_${info_key}_${redis_port}
 fi
